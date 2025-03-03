567793faf4ef0eb2865e238bfcc1e2c1c97e91b1
Dilip <dilipk1261@gmail.com>
2025-03-03 23:27:04 +0530

    Cart Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Order from "./pages/order/Order";
 import { ContextProvider } from "./context/Context";
+import Login from "./pages/registration/Login";
+import Signup from "./pages/registration/Signup";
+import Cart from "./pages/cart/Cart";
+import NoPage from "./pages/nopage/NoPage";
+import ProductInfo from "./pages/productinfo/ProductInfo";
 
 function App() {
   return (
@@ -12,9 +17,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/order" element={<Order />} />
-          <Route path="/cart" element={<Home />} />
+          <Route path="/cart" element={<Cart/> } />
           <Route path="/dashboard" element={<Home />} />
-          <Route path="/*" element={<Home />} />
+          <Route path="/login" element={<Login/> } />
+          <Route path="/signup" element={<Signup/> } />
+          <Route path="/productinfo/:id" element={<ProductInfo/> } />
+          <Route path="/*" element={<NoPage/> } />
         </Routes>
       </Router>
     </ContextProvider>
